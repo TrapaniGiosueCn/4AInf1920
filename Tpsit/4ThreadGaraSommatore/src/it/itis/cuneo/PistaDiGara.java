8623b5dec9cabfdcf1baf771417c36976c4840ef
TrapaniGiosueCn <57175208+TrapaniGiosueCn@users.noreply.github.com>
2020-02-06 11:25:51 +0100

    Update PistaDiGara.java

--- Tpsit/4ThreadGaraSommatore/src/it/itis/cuneo/PistaDiGara.java
+++ Tpsit/4ThreadGaraSommatore/src/it/itis/cuneo/PistaDiGara.java
@@ -1,22 +1,11 @@
 package it.itis.cuneo;
 
-/**
- * Created by inf.trapanig0312 on 30/01/2020.
- */
-public class PistaDiGara {
+public class PistaDiGara{
     public static void main(String[] args) {
-        Corridore corridore1 = new Corridore("Fabio");
-        Corridore corridore2 = new Corridore("Enrico");
+        Numero numero1 = new Numero(5);
+        Numero numero2 = new Numero(4);
 
-        Runnable corridoreRun1Runnable = new CorridoreInterfaccia("MACCHIAVELLI");
-        Thread corridoreRun1 = new Thread(corridoreRun1Runnable);
-
-        Runnable corridoreRun2Runnable = new CorridoreInterfaccia("BOCCACCIO");
-        Thread corridoreRun2 = new Thread(corridoreRun2Runnable);
-
-        corridore1.start();
-        corridore2.start();
-        corridoreRun1.start();
-        corridoreRun2.start();
+        numero1.start();
+        numero2.start();
     }
 }
